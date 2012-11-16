763c319c9107a0aed5a93277f1fda271fac777f9
Mara <mara.matias1@gmail.com>
2012-11-16 03:12:17 +0000

    update: multiscale entropy and compression calculations have been moved to the multiscale module

--- HRFAnalyseMultiScale.py
+++ HRFAnalyseMultiScale.py
@@ -31,10 +31,10 @@ if __name__=="__main__":
     if input_dir.endswith('/'):
         input_dir = input_dir[:-1]
     
-    scales_dir = input_dir+'_Escalas'
+    scales_dir = input_dir+'_Scales'
 
     print "Creating Scales Directory"
-    tools.multiscale.multiscale(input_dir,scales_dir,options["scale_start"],options["scale_stop"]+1,options["scale_step"])
+    tools.multiscale.create_scales(input_dir,scales_dir,options["scale_start"],options["scale_stop"]+1,options["scale_step"])
     print "Scales Directory created"
 
     if options["command"]=="compress":
@@ -44,23 +44,13 @@ if __name__=="__main__":
         else:
             outfile="%s_multiscale_%d_%d_%d_%s%s.csv"%(input_dir, options["scale_start"], options["scale_stop"], options["scale_step"], options["compressor"], options["level"])
 
-        compression_table={}
-        for scale in xrange(options["scale_start"],options["scale_stop"]+1,options["scale_step"]):
-            dir_to_compress=os.path.join(scales_dir,"Escala %d"%scale)
-
-            compression_results = tools.compress.compress(dir_to_compress,options["compressor"],options["level"],options['decompress'])
-            if compression_table=={}:
-                if options['decompress']:
-                    compression_table= dict((k,[compression_results[k].original,compression_results[k].compressed,compression_results[k].time]) for k in compression_results)
-                else:
-                    compression_table= dict((k,[compression_results[k].original,compression_results[k].compressed]) for k in compression_results)
-            else:
-                for k in compression_table:
-                    compression_table[k].append(compression_results[k].original)
-                    compression_table[k].append(compression_results[k].compressed)
-                    if options['decompress']:
-                        compression_table[k].append(compression_results[k].time)
-        
+        compression_table = tools.multiscale.multiscale_compression(input_dir,
+                                                                    options["scale_start"],
+                                                                    options["scale_stop"]+1,
+                                                                    options["scale_step"],
+                                                                    options["compressor"],
+                                                                    options["level"],
+                                                                    options["decompress"])
 
         writer=csv.writer(open(outfile,"wb"),delimiter=";")
         if options['decompress']:
@@ -71,46 +61,32 @@ if __name__=="__main__":
         for filename in sorted(compression_table.keys()):
             writer.writerow([filename]+compression_table[filename])
         
-    else:
-        if (options['entropy']=='apen') or (options['entropy']=='sampen'):
+    elif options["command"]=="entropy":
+        if (options['entropy']=='apen' or options['entropy']=="sampen"):
             outfile="%s_multiscale_%d_%d_%d_%s%d%.2f.csv"%(input_dir, 
-                                                         options["scale_start"], 
-                                                         options["scale_stop"], 
-                                                         options["scale_step"], 
-                                                         options["entropy"], 
-                                                         options["dimension"],
-                                                         options["tolerance"])
+                                                           options["scale_start"], 
+                                                           options["scale_stop"], 
+                                                           options["scale_step"], 
+                                                           options["entropy"], 
+                                                           options["dimension"],
+                                                           options["tolerance"])
             entropy_table={}
-            for scale in xrange(options["scale_start"],options["scale_stop"]+1,options["scale_step"]):
-                scale_dir=os.path.join(scales_dir,"Escala %d"%scale)
-                entropy_results = tools.entropy.entropy(scale_dir, 
-                                              options['entropy'],
-                                              options['dimension'],
-                                              options['tolerance'])
-                if entropy_table=={}:
-                    entropy_table= dict((k,[entropy_results[k][1]]) for k in entropy_results)
-                else:
-                    for k in entropy_table:
-                        entropy_table[k].append(entropy_results[k][1])
-
+            
+            entropy_table = tools.multiscale.multiscale_entropy(input_dir,
+                                                                options["scale_start"],
+                                                                options["scale_stop"]+1,
+                                                                options["scale_step"],
+                                                                options["entropy"],
+                                                                options["dimension"],
+                                                                options["tolerance"])
+            
+            
             writer=csv.writer(open(outfile,"wb"),delimiter=";")
             header = ["Filename"]+[ "Escala%d Entropy"%s for s in xrange(options["scale_start"],options["scale_stop"]+1,options["scale_step"])]
-        writer.writerow(header)
-        for filename in sorted(entropy_table.keys()):
-            writer.writerow([filename]+entropy_table[filename])
+            writer.writerow(header)
+            for filename in sorted(entropy_table.keys()):
+                writer.writerow([filename]+entropy_table[filename])            
+        else:
+            print "Multiscale not implemented for %s"%options["entropy"]
 
-            
-
-        # if options['entropy']=="specen":
-        #     resulting_dict = entropy_dir(inputdir,options['entropy'],Band,Fs)
-        #     outfile= inputdir+'_'+options['entropy']+'.csv'
-        # if options['entropy']=="hurst" or options['entropy']=="dfa" or options['entropy']=="hjorth" or  options['entropy']=="pfd":
-        #     resulting_dict = entropy_dir(inputdir,options['entropy'])
-        #     outfile= inputdir+'_'+options['entropy']+'.csv'
-        # if options['entropy']=="hfd":
-        #     resulting_dict = entropy_dir(inputdir,options['entropy'],options['kmax'])
-        #     outfile= inputdir+'_'+options['entropy']+'_'+str(options['kmax'])+'.csv'
-        # if options['entropy']=='fi' or options['entropy']=='svden':
-        #     resulting_dict = entropy_dir(inputdir,options['entropy'],options['dimension'],options['tau'])
-        #     outfile= inputdir+'_'+options['entropy']+'_'+str(options['dimension'])+'_'+str(options['tau'])+'.csv'
-        
+    

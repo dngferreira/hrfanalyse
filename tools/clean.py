fff8ad0dbda05ac5b027f8af1e8b86e40ab72b56
Mara <mara.matias1@gmail.com>
2013-03-29 18:18:10 +0000

    update: if we are using raw SISPorto files keep the int value for the hrf; documentation update

--- tools/clean.py
+++ tools/clean.py
@@ -20,14 +20,21 @@ This file is part of HRFAnalyse.
 _______________________________________________________________________________
 
 
-This module is very specific to this application. It either extracts
-the time stamp and heart rate frequency(hrf) or just the hrf. 
+This module is very specific to our datasets. The dataset files are always either
+two columns -- the first is the time and the second the heat rate frequency or rr 
+interval -- or more columns where only the first two interset us --again the time
+and hrf wich in these files has to be divided by 1000 to get the usual three digit
+number.
+
+Dependign on the passed options the module either extracts the time stamp and 
+heart rate frequency(hrf) or just the hrf. 
 
 Optionaly a limit may be applied to eliminate signal loss, we consider the
 signal to be lost if hrf is bellow 50 or above 250. If a particular line is 
 considered as signal lost it is ommited from the resulting file.
 
-This module's entry point function in clean(...)
+This module's entry point function in clean(input_name,dest_dir,keep_time=False,
+apply_limits=False)
 """
 
 import os
@@ -42,52 +49,45 @@ def clean(input_name,dest_dir,keep_time=False, apply_limits=False):
     Cleans the file or every file from a directory named input_name,
     and saves the resulting files in dest_dir
 
-    Arguments: Input directory; output directory.  
+    ARGUMENTS: Strin Input directory, String output directory, bool to decide 
+    whether or not to keep the time column, bool to determine if limits(50-250) 
+    are applied.  keep_time and apply_limits are optional to call this function.
 
-    Optional arguments: keep_time, if true the time stamp will be kept
-    along with hrf(False by default); apply_limits if true signal loss
-    will be eliminated(False by default).
-
-    Return: None
-
-    Algorithm: If input_name is a directory, retrieve a complete list
-    of files from input directory. Call the clean function for each of
-    the file.
+    RETURN: None
 
     """
-    module_logger.debug("The dir name received: %s"%input_name)
+    module_logger.debug("The input name received: %s"%input_name)
     if os.path.isdir(input_name):
         filelist = os.listdir(input_name)
         for filename in filelist:
-            clean_file(os.path.join(input_name,filename.strip()), os.path.join(dest_dir,filename.strip()), keep_time, apply_limits)
+            clean_file( os.path.join(input_name,filename.strip()), 
+                        os.path.join(dest_dir,filename.strip()), 
+                        keep_time, 
+                        apply_limits)
     else:
         filename= os.path.basename(input_name)
-        clean_file(input_name, os.path.join(dest_dir,filename.strip()), keep_time,apply_limits)
+        clean_file( input_name, 
+                    os.path.join(dest_dir,filename.strip()), 
+                    keep_time,
+                    apply_limits)
 
 #IMPLEMENTATION
 
 def clean_file(inputfile, dest_file , keep_time, apply_limits):
     """
-    Create a file based on the input file where only hrf (where hrf
-    are transformed, the hrf saved is the division of the original hrf
-    by 1000), and optionally timestamp are kept.
-
-    Arguments: name of input file, name of output file, a boolean
-    indication whether timestamps should be preserved, a boolean that
-    indicates if signal loss should be eliminated or not.
+    Clean operation of a single file.
     
-    Return: None
+    ARGUMENTS: String name of input file, String name of output file, keep_time 
+    boolean indication whether timestamps should be preserved, apply_limits boolean
+    that indicates if signal loss should be eliminated or not.
     
-    Algorithm: The input file is read and the heart rate column, and
-    possibly the timestamp column, are written to the outpufile. If
-    apply_limits is true then the line is only written if hrf is
-    between 50 and 250, otherwise we jump to the next line.
-
+    RETURN: None
+    
     """
     with open(inputfile,"rU") as fdin:
         with open(dest_file,"w") as fdout:
             for line in fdin:
-                data =  line.split()
+                data =  line.split(',')
                 
                 #to clean any headers the file might have, this operates under the assumtion
                 #headers never start with a number.
@@ -98,7 +98,7 @@ def clean_file(inputfile, dest_file , keep_time, apply_limits):
                 if len(data)!=0:
                     hrf = float(data[1])
                     if(hrf >= 1000):
-                        hrf = float(data[1])/1000
+                        hrf = round(float(data[1])/1000)
                     if apply_limits and hrf>= 50 and hrf<=250:
                         if keep_time:
                             time = data[0]
@@ -120,12 +120,21 @@ def add_parser_options(parser):
     parser or subparser, and are the optional arguments for
     the entry function in this module
 
-    Arguments: The parser to which you want the arguments added to.
+    ARGUMENTS: The parser to which you want the arguments added to.
     
-    Return:None
+    RETURN:None
     """
 
-    parser.add_argument("-kt","--keep-time",dest="keep_time",action="store_true",default=False,help="When cleaning keep both the hrf and time stamp")
-    parser.add_argument("--apply-limits", dest="apply_limits", action="store_true", default=False,help="When cleaning apply limit cutoffs")
+    parser.add_argument("-kt",
+                        "--keep-time",
+                        dest="keep_time",
+                        action="store_true",
+                        default=False,
+                        help="When cleaning keep both the hrf and time stamp")
+    parser.add_argument("--apply-limits", 
+                        dest="apply_limits", 
+                        action="store_true", 
+                        default=False,
+                        help="When cleaning apply limit cutoffs")
 
 

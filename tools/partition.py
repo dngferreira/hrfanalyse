b181276e0c472aa7ea9b7ce2b505b1dbc6d90c73
Mara <mara.matias1@gmail.com>
2013-09-08 19:58:18 +0100

    documentation update; alterations to the rewritten module to return the real partition times

--- tools/partition.py
+++ tools/partition.py
@@ -19,23 +19,18 @@ This file is part of HRFAnalyse.
 
 _______________________________________________________________________________
 
-This module presumes the files contain only two columns, the first
-column is a timestamp the second is the heart rate frequency(hrf).
+!!!IMPLEMETNTATION NOTE: This module presumes the files contain only two columns, the first
+column is a time series the second is the heart rate frequency(hrf).
 
-Timestamps can either be the cummulative passage of time, or just 
-the time passed since the last timestamp.
+The time series can either be a cummulative passage of time, or timestamps where
+each stamp indicates the time passed since the last timestamp.
 
-The partitions using time stamps(not cummulative) do not use the real
-time, but the mode of time this way the data is not affected by
+The partitions using time stamps do not use the actual value of the timestamp, it
+uses the mode of all timestamps this way the data is not affected by
 extended periods of signal loss, and what we get is closer to the
-period of aquired signal.
+period of aquired signal. !!!
 
-There are two entry point functions to this module:
- partition_chunck(...) --> Grabs a chunck from the file /files in a
- directory 
- 
- partition_blocks(...) --> Partitions the file / files in a directory
- into sections
+ENTRY POINT:partition(input_name,dest_dir,starting_point=0,section=-1,gap=-1,start_at_end=False,full_file=False,lines=False)
 
 """
 
@@ -55,9 +50,10 @@ def partition(input_name,dest_dir,starting_point=0,section=-1,gap=-1,start_at_en
     """
     (str,str,int,int,int,bool,bool,bool) -> ( dict of str: list of tuples(float, float))
 
-    Partition all the file in input_name using cutting_method(lines or time), start the partition with size interval
-    at starting_point, if start_at_end is True partition from the file\'s end. Create a file for the partition in
-    dest_dir.
+    Partition all the file in input_name, start the first cut at at starting_point and cut a section sized chunck of the
+    file, if full_file option is activated cut the hole file into sections, where each section(si) starts at starting_point+gap*si.
+    If start_at_end is used partition from the file\'s end. Partitions can be done by time(default) or by number of lines (
+    by activating the lines option).
 
     """
     block_times={}
@@ -71,14 +67,17 @@ def partition(input_name,dest_dir,starting_point=0,section=-1,gap=-1,start_at_en
         block_times[filename] = partition_file(input_name.strip(),dest_dir,starting_point,section,gap,start_at_end,full_file,lines)
     return block_times
 
+#IMPLEMENTATION
 
 def partition_file(input_name, dest_dir, starting_point, section, gap, start_at_end, full_file, lines):
     """
-    (str,str,str,int,int,bool) -> list of tuples (float, float)
+    (str,str,int,int,int,bool, bool, bool) -> list of tuples (float, float)
 
-    Partition a single file using cutting_method(lines or time), start the partition with size interval
-    at starting_point, if start_at_end is True partition from the file\'s end. Create a file for the partition in
-    dest_dir.
+    Partition a single file, start the first partition at starting_point, and cut a section sized chunck of the
+    file, if full_file option is activated cut the hole file into sections, where each section(si) starts at starting_point+gap*si.
+    If start_at_end is used partition from the file\'s end. Partitions can be done by time(default) or by number of lines (
+    by activating the lines option)
+    
     """
     if lines:
         return partition_by_lines(input_name, dest_dir, starting_point, section, gap, start_at_end, full_file)
@@ -91,33 +90,55 @@ def partition_by_lines(input_name, dest_dir, starting_point, section, gap, start
     (str, str, int, int, int, bool, bool) --> list of tuples (float, float)
 
 
-    Partition a single file using the number of lines to measure the size of the partition.
+    Partition a single file using the number of lines to measure the size of the partition. Returns the real
+    (not just aquired signal time) times for beginning and end of the partitions.
     """
 
     with open(input_name,'rU') as fdin:
         lines = fdin.readlines()
     lines = [line for line in lines if line!="\n"]
     filename = os.path.splitext(os.path.basename(input_name))[0]
+    if start_at_end:
+        cumulative, time_stamp = sniffer(lines[-SAMPLE_SIZE:], start_at_end)
+    else:
+        cumulative, time_stamp = sniffer(lines[:SAMPLE_SIZE], start_at_end)
+    p_init, p_end = initial_indexes_lines(starting_point, section,start_at_end, len(lines)-1)
+    r_start = get_p_rtime(lines[:p_init+1],0,cumulative)
+    r_end = get_p_rtime(lines[p_init:p_end],r_start,cumulative)
+    p_times=[]
     if full_file:
         k=1
         file_block_dir = os.path.join(dest_dir,"%s_blocks"%filename)
         if(not os.path.isdir(file_block_dir)):
             module_logger.info("Creating %s!"%file_block_dir)
             os.makedirs(file_block_dir)
-        p_init, p_end = initial_indexes_lines(starting_point, section,False, len(lines)-1)
         while p_end < len(lines):
             partname = "%s_%d"%(filename,k)
             write_partition(lines, os.path.join(file_block_dir,partname),p_init,p_end)
+            p_times.append((r_start,r_end))
             k+=1
+            r_start = get_p_rtime(lines[p_end:p_end+1],r_end,cumulative)
             p_init, p_end = next_indexes_lines(p_init,p_end,gap)
+            r_end = get_p_rtime(lines[p_init:p_end],r_start,cumulative)
         partname = "%s_%d"%(filename,k)
         write_partition(lines, os.path.join(file_block_dir,partname),p_init,len(lines))
+        p_times.append((r_start,r_end))
     else:
-        p_init, p_end = initial_indexes_lines(starting_point, section,start_at_end, len(lines)-1)
         write_partition(lines, os.path.join(dest_dir,filename),p_init,p_end)
-    return []
+        p_times.append((r_start,r_end))
+    return p_times
+
+
 
 def initial_indexes_lines(starting_point, section, start_at_end, total_len):
+    """
+    (int,int,bool,int) -> (int, int)
+
+    Return the initial and final indexes for the first partition.
+    These indexes are calculated by assuming each line in the file as an array position.
+    First line in the file is line 0, last line in the file is total_len.
+
+    """
     if start_at_end:
         p_init = total_len-section
         p_end = total_len-starting_point
@@ -127,12 +148,43 @@ def initial_indexes_lines(starting_point, section, start_at_end, total_len):
     return int(p_init), int(p_end)
 
 def next_indexes_lines(p_init,p_end, gap):
+    """
+    (int, int, int) -> (int, int)
+
+    Take the last partitions initial and final indexes and return the next partition's indexes.
+    
+    """
     p_init += gap
     p_end += gap
     return int(p_init), int(p_end)
 
+def get_p_rtime(lines, real_s, cumulative):
+    """
+    (list, float, bool) -> float
+    
+    Calculate the real time at the end of the partition. The data in the partition is passed as a list,
+    and the time at the start of this partition is passed as an argument.    
+    """
+    r_time = real_s
+    for line in lines:
+        try:
+            time, hrf = line.split()
+        except ValueError:
+            time = 0
+        if cumulative:
+            r_time = float(time)
+        else:
+            r_time += float(time)
+    return r_time
 
 def partition_by_time(input_name, dest_dir, starting_point, section, gap, start_at_end, full_file):
+    """
+    (str, str, int, int, int, bool, bool) --> list of tuples (float, float)
+
+
+    Partition a single file using the elapsed time to measure the size of the partition. Returns the real
+    (not just aquired signal time) times for beginning and end of the partitions.
+    """
     with open(input_name,'rU') as fdin:
         lines= fdin.readlines()
     lines = [line for line in lines if line!="\n"]
@@ -141,39 +193,50 @@ def partition_by_time(input_name, dest_dir, starting_point, section, gap, start_
         cumulative, time_stamp = sniffer(lines[-SAMPLE_SIZE:], start_at_end)
     else:
         cumulative, time_stamp = sniffer(lines[:SAMPLE_SIZE], start_at_end)
+    p_init, p_end = initial_indexes_time(lines, starting_point, section, start_at_end, cumulative, time_stamp)
+    r_start = get_p_rtime(lines[:p_init+1],0,cumulative)
+    r_end = get_p_rtime(lines[p_init:p_end],r_start,cumulative)
+    p_times=[]
     if full_file:
         k=1
         file_block_dir = os.path.join(dest_dir,"%s_blocks"%filename)
         if(not os.path.isdir(file_block_dir)):
             module_logger.info("Creating %s!"%file_block_dir)
             os.makedirs(file_block_dir)
-        p_init, p_end = initial_indexes_time(lines, starting_point, section, False, cumulative, time_stamp)
         while p_end < len(lines):
             partname = "%s_%d"%(filename,k)
             write_partition(lines, os.path.join(file_block_dir,partname),p_init,p_end)
+            p_times.append((r_start,r_end))
             k+=1
+            r_start = get_p_rtime(lines[p_end:p_end+1],r_end,cumulative)
             p_init, p_end = next_indexes_time(lines, p_init, p_end, gap, section, cumulative, time_stamp)
-
+            r_end = get_p_rtime(lines[p_init:p_end],r_start,cumulative)
         partname = "%s_%d"%(filename,k)
         write_partition(lines, os.path.join(file_block_dir,partname),p_init,p_end)
- 
+        p_times.append((r_start,r_end))
     else:
-        p_init, p_end = initial_indexes_time(lines, starting_point, section,start_at_end, cumulative, time_stamp)
         write_partition(lines, os.path.join(dest_dir,filename),p_init,p_end)
-    return []
+        p_times.append((r_start,r_end))
+    return p_times
 
 def next_indexes_time(lines, p_init, p_end, gap, section, cumulative, time_stamp):
+    """
+    (int, int, int) -> (int, int)
+
+    Take the last partitions initial and final indexes and return the next partition's indexes.
+    
+    """
     time_elapsed=0
     time, hrf =lines[p_init].split()        
     if cumulative:
         time_stamp = float(time)
-    while test_time_limit(cumulative, time, time_stamp, time_elapsed, gap):
+    while test_time_limit(cumulative, float(time), time_stamp, time_elapsed, gap):
         p_init +=1
         time, hrf = lines[p_init].split()
         if not cumulative:
            time_elapsed += time_stamp
         
-    while test_time_limit(cumulative, time, time_stamp, time_elapsed, gap+section) and abs(p_end) < len(lines):
+    while test_time_limit(cumulative, float(time), time_stamp, time_elapsed, gap+section) and abs(p_end) < len(lines):
         p_end +=1
         if p_end== len(lines):
             break
@@ -186,6 +249,12 @@ def next_indexes_time(lines, p_init, p_end, gap, section, cumulative, time_stamp
     
 
 def initial_indexes_time(lines, starting_point, section, start_at_end, cumulative, time_stamp):
+    """
+    (int,int,bool,int) -> (int, int)
+
+    Return the initial and final indexes for the first partition.
+
+    """
     if start_at_end:
         aux_index=-1
     else:
@@ -194,7 +263,7 @@ def initial_indexes_time(lines, starting_point, section, start_at_end, cumulativ
     time_elapsed=0
     time, hrf = lines[aux_index].split()
 
-    while test_time_limit(cumulative, time, time_stamp, time_elapsed, starting_point) and abs(aux_index) < len(lines):
+    while test_time_limit(cumulative, float(time), time_stamp, time_elapsed, starting_point) and abs(aux_index) < len(lines):
         if start_at_end:
             aux_index -=1
         else:
@@ -209,7 +278,7 @@ def initial_indexes_time(lines, starting_point, section, start_at_end, cumulativ
         p_init = aux_index
 
         
-    while test_time_limit(cumulative, time, time_stamp, time_elapsed, starting_point+section) and abs(aux_index) < len(lines):
+    while test_time_limit(cumulative, float(time), time_stamp, time_elapsed, starting_point+section) and abs(aux_index) < len(lines):
         if start_at_end:
             aux_index -=1
         else:
@@ -233,6 +302,17 @@ def initial_indexes_time(lines, starting_point, section, start_at_end, cumulativ
         
 
 def test_time_limit(cumulative, time, time_stamp, time_elapsed, desired_time):
+    """
+    (bool, float, float, float, float) -> bool
+
+    Auxilary function to help test a current time agains a spipulated time limit.
+
+    If the time series is cumulative then we test how much time has passed by
+    subtracting the last time read from the inital time read at the beginning of the
+    partition.
+    If the time series is not cumulative then time_elapsed keep track of how much
+    time has passed since the beginning of the partition.
+    """
     if cumulative:
         current_time = abs(float(time) - time_stamp)
         return current_time < desired_time
@@ -242,6 +322,12 @@ def test_time_limit(cumulative, time, time_stamp, time_elapsed, desired_time):
     
     
 def write_partition(lines, output_file, i_index, f_index):
+    """
+    (list, str, int, int) -> NoneType
+
+    Take the hrf file contents ( in a list) and write the lines from a partition to
+    output_file.
+    """
     with open(output_file,"w") as fdout:
         while i_index < f_index:
             try:
@@ -259,7 +345,8 @@ def write_partition(lines, output_file, i_index, f_index):
 
 def sniffer(lines,start_at_end=False):
     """
-    !!!Auxiliary function!!!
+    (list, bool) -> (bool, float)
+
     Recieves a sample of the file and extrapolates whether the
     timeline is cumulative or periodic. If it's cumulative than along
     with that information also sends the first timestamp, otherwise it
@@ -267,12 +354,11 @@ def sniffer(lines,start_at_end=False):
     always capture the signal on a time frequency, the mode will give
     us that frequency).
 
-    Arguments: sample data in an array.
-    
-    Return: a boolean that indicates if we have a cumulative timeline
-    or not, and the first time stamp or the mode time stamp.
-
     """
+    # If the file only has the hrf column it can still be partitioned by lines,
+    # the values returned in that case are irrelevant
+    if len(lines[0].split())<2:
+        return (False, 0)
     times={}
     crescent=0
     if start_at_end:
@@ -302,14 +388,20 @@ def sniffer(lines,start_at_end=False):
         time_stamp=moda
     return (cumulative,float(time_stamp))
 
-def add_parser_options(parser, full_file_option):
+def add_parser_options(parser, full_file_option=True):
     """
+     (argparse.ArgumentParser, bool) -> NoneType
+
+    !!!Auxiliary function!!!  These are arguments for an argparse
+    parser or subparser, and are the parameters taken by the entry function 
+    in this module. The full_file_option disables/enables the presence of the
+    full_file option.
     """
 
     parser.add_argument("--start-at-end",dest="start_at_end",action="store_true",default=None,help="Partition from end of file instead of beginning")
     parser.add_argument("-ds","--defered-start",dest="partition_start",metavar="SECONDS",action="store",type=float,default=0,help="Time gap between the start of the file and the start of the interval; default:[%(default)s]")
     parser.add_argument("-s","--section",dest="section",metavar="SECONDS",action="store",type=float,help="Amount of time in seconds to be captured")
-    parser.add_argument("-g", "--gap", dest="gap", metavar="SECONDS", type=float, action="store", help="gap between sections (if using --full-file option)", default=0)
+    parser.add_argument("-g", "--gap", dest="gap", metavar="SECONDS", type=float, action="store", help="gap between sections", default=0)
     parser.add_argument("--use-lines",dest="using_lines", action="store_true", default=False,help="Partition using line count instead of time")
     if full_file_option:
         parser.add_argument("--full-file", dest="full_file", action="store_true", default=False, help="Partition the full file into blocks")
